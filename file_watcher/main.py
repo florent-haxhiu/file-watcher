20cfa7dd9e7440423fef4cdaddb3579e78b64c7d
Florent Haxhiu <florenthaxhiu03@gmail.com>
2024-11-09 13:47:12 +0000

    add: created file watcher
    want to ignore all git stuff tbh

---
+++ file_watcher/main.py
@@ -0,0 +1,141 @@
+import logging
+import time
+from collections.abc import Callable
+from dataclasses import dataclass
+from pathlib import Path
+
+from watchdog.events import (
+    DirCreatedEvent,
+    DirDeletedEvent,
+    DirModifiedEvent,
+    FileCreatedEvent,
+    FileDeletedEvent,
+    FileModifiedEvent,
+    FileSystemEventHandler,
+)
+from watchdog.observers import Observer
+
+
+@dataclass
+class FileState:
+    """Stores metadata about a file"""
+
+    modified_time: float
+    size: int
+    hash: str = ""
+
+
+class CustomEventHandler(FileSystemEventHandler):
+    def __init__(self, callbacks: dict[str, Callable]) -> None:
+        self._callbacks = callbacks
+        super().__init__()
+
+    def on_created(self, event: DirCreatedEvent | FileCreatedEvent) -> None:
+        if not event.is_directory:
+            self._callbacks["created"](event.src_path)
+
+    def on_modified(self, event: DirModifiedEvent | FileModifiedEvent) -> None:
+        if not event.is_directory:
+            self._callbacks["modified"](event.src_path)
+
+    def on_deleted(self, event: DirDeletedEvent | FileDeletedEvent) -> None:
+        if not event.is_directory:
+            self._callbacks["deleted"](event.src_path)
+
+
+class FileSystemWatcher:
+    def __init__(self, directory: str, patterns: set[str] | None = None) -> None:
+        self.directory = Path(directory)
+        self.patterns = patterns or {"*"}  # Defaults to watching all files
+        self.file_state: dict[str, FileState] = {}
+        self._logger = self._setup_logger()
+
+        self._callbacks = {
+            "created": self._handle_created,
+            "modified": self._handle_modified,
+            "deleted": self._handled_deleted,
+        }
+
+        self.event_handler = CustomEventHandler(self._callbacks)
+        self.observer = Observer()
+        self.observer.schedule(self.event_handler, str(self.directory), recursive=True)
+
+    def _setup_logger(self) -> logging.Logger:
+        """Configure logger for the file watcher"""
+        logger = logging.getLogger("FileWatcher")
+        logger.setLevel(logging.INFO)
+
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter(
+            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        )
+
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+        return logger
+
+    def _get_file_state(self, file_path: str) -> FileState | None:
+        """Get current state of a file"""
+        path = Path(file_path)
+        if path.exists():
+            return FileState(
+                modified_time=path.stat().st_mtime, size=path.stat().st_size
+            )
+        return None
+
+    def _handle_created(self, file_path: str) -> None:
+        """Handle file creation events"""
+        self._logger.info(f"File created: {file_path}")
+        if file_state := self._get_file_state(file_path):
+            self.file_state[file_path] = file_state
+
+    def _handle_modified(self, file_path: str) -> None:
+        """Handle file modifications events"""
+        new_state = None
+        if file_state := self._get_file_state(file_path):
+            new_state = file_state
+        else:
+            return
+
+        if file_path in self.file_state:
+            old_state = self.file_state[file_path]
+            if new_state.modified_time != old_state.modified_time:
+                self._logger.info(f"File modified: {file_path}")
+                self._logger.info(
+                    f"Size changed from {old_state.size} to {new_state.size} bytes"
+                )
+        self.file_state[file_path] = new_state
+
+    def _handled_deleted(self, file_path: str) -> None:
+        """Handle file deletion events"""
+        self._logger.info(f"File deleted: {file_path}")
+        self.file_state.pop(file_path, None)
+
+    def start(self):
+        """Start watching the directory"""
+        self._logger.info(f"Starting to watch the directory: {self.directory}")
+        self.observer.start()
+        try:
+            while True:
+                time.sleep(1)
+        except KeyboardInterrupt:
+            self.stop()
+
+    def stop(self):
+        """Stop watching the directory"""
+        self.observer.stop()
+        self.observer.join()
+        self._logger.info("Stopped watching directory")
+
+
+if __name__ == "__main__":
+    import sys
+
+    args = sys.argv
+    dir = args[1]
+    watcher = FileSystemWatcher(dir)
+    try:
+        watcher.start()
+    except KeyboardInterrupt:
+        watcher.stop()
